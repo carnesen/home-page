0c0ba09615999ed96d3768eb8e372c688e495b52
Chris Arnesen <chris.arnesen@gmail.com>
2018-10-23 14:42:47 -0500

    Simplify for use with @carnesen/google-cloud

--- scripts/build.js
+++ scripts/build.js
@@ -4,27 +4,21 @@
 const path = require('path')
 const fs = require('fs-extra')
 
-const {getGitHash} = require('@carnesen/aws')
-
 const topDir = path.resolve(__dirname, '..')
 const distDir = path.join(topDir, 'dist')
 const publicDir = path.join(topDir, 'public')
 
-function copyToDist(sourcePath) {
+function copyToDist (sourcePath) {
   const fileName = path.basename(sourcePath)
   console.log(`  - ${fileName}`)
   const destinationPath = path.join(distDir, fileName)
   fs.copySync(sourcePath, destinationPath)
 }
 
-const sourcePaths = fs.readdirSync(publicDir)
-  .map(fileName => path.join(publicDir, fileName))
-
 console.log('Emptying dist/')
 fs.emptyDirSync(distDir)
 
 console.log('Copying files:')
+const sourcePaths = fs.readdirSync(publicDir)
+  .map(fileName => path.join(publicDir, fileName))
 sourcePaths.forEach(copyToDist)
-
-fs.writeFileSync(path.join(distDir, 'version'), getGitHash())
-console.log('Wrote version file')
--- scripts/deploy.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-const {websiteFactory, run} = require('@carnesen/aws')
-
-const website = websiteFactory()
-
-run(website.create)

07d89ef786b7072ebf5cc60e3ce7c236b8046cad
Chris Arnesen <chris.arnesen@gmail.com>
2019-01-11 11:10:10 +0700

    Upgrade to next.js (#2)

--- scripts/build.js
+++ scripts/build.js
@@ -1,23 +1,19 @@
-'use strict'
+const path = require('path');
+const fs = require('fs-extra');
 
-const path = require('path')
-const fs = require('fs-extra')
+const topDir = path.resolve(__dirname, '..');
+const distDir = path.join(topDir, 'dist');
+const publicDir = path.join(topDir, 'public');
 
-const topDir = path.resolve(__dirname, '..')
-const distDir = path.join(topDir, 'dist')
-const publicDir = path.join(topDir, 'public')
-
-function copyToDist (sourcePath) {
-  const fileName = path.basename(sourcePath)
-  console.log(`  - ${fileName}`)
-  const destinationPath = path.join(distDir, fileName)
-  fs.copySync(sourcePath, destinationPath)
+function copyToDist(sourcePath) {
+  const fileName = path.basename(sourcePath);
+  console.log(`  - ${fileName}`);
+  const destinationPath = path.join(distDir, fileName);
+  fs.copySync(sourcePath, destinationPath);
 }
 
-console.log('Emptying dist/')
-fs.emptyDirSync(distDir)
-
-console.log('Copying files:')
-const sourcePaths = fs.readdirSync(publicDir)
-  .map(fileName => path.join(publicDir, fileName))
-sourcePaths.forEach(copyToDist)
+console.log('Copying files:');
+const sourcePaths = fs
+  .readdirSync(publicDir)
+  .map(fileName => path.join(publicDir, fileName));
+sourcePaths.forEach(copyToDist);
--- src/home/index.jsx
+++ src/home/index.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-import me from './me.jpg'
-import styles from './styles.css'
+import styles from './styles.css';
 
-export default function Home () {
+export default function Home() {
   return (
     <div>
       <div className={styles.wrapper}>
         <div className={styles.imageWrapper}>
-          <img src={me} className={styles.image} />
+          <img src="/static/images/me.jpg" className={styles.image} alt="me" />
         </div>
         <div className={styles.titleWrapper}>
-          <a href='/documents/Chris_Arnesen.pdf'>
+          <a href="/static/documents/Chris_Arnesen.pdf">
             <h1>Chris Arnesen</h1>
             <h3>Web Engineer</h3>
           </a>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-Home.propTypes = {}
+Home.propTypes = {};

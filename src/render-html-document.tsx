458d0887f19b80a992c641d102472d4ca71d8b3b
Chris Arnesen <chris.arnesen@gmail.com>
2020-06-21 21:57:44 -0700

    Remove Google Analytics from browser and server
    Publish 2020.6.0

--- src/render-html-document.tsx
+++ src/render-html-document.tsx
@@ -8,15 +8,6 @@ export function renderHtmlDocument(element: React.ReactElement): string {
     <head>
       <meta charset="utf-8">
       <title>Chris Arnesen's Website</title>
-      <!-- Global site tag (gtag.js) - Google Analytics -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-101920219-2"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-101920219-2');
-      </script>
       <link rel="stylesheet" type="text/css" href="/main.css">
     </head>
     <body>
--- src/router-middleware.tsx
+++ src/router-middleware.tsx
@@ -3,13 +3,11 @@ import Koa = require('koa');
 import { OK } from 'http-status-codes';
 import { MainPage } from './main-page';
 import { renderHtmlDocument } from './render-html-document';
-import { track } from './util';
 
 export const routerMiddleware: Koa.Middleware = async (ctx, next) => {
   if (ctx.method === 'GET' && ctx.path === '/') {
     ctx.response.status = OK;
     ctx.body = renderHtmlDocument(<MainPage />);
-    track('pages', 'view', 'main');
   } else {
     await next();
   }
